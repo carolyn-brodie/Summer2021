c651decb9c8583f8f11a18d78816a9f55b514a38
larakallem <lara.kallem@my.simpson.edu>
2021-06-16 11:47:27 -0500

    R Files for Graphs

--- summer_work/Graphs/BMEAll.R
+++ summer_work/Graphs/BMEAll.R
@@ -1,22 +1,18 @@
 
-
-plotBME <- function(sound) {
+plotBME <- function() {
    library(dplyr)
-  library(ggplot2)
- library(stringr)
- library(ggeasy)
+   library(ggplot2)
+   library(stringr)
+   library(ggeasy)
 
 incorrectData <- read.csv("/Users/larakallem/PycharmProjects/Summer2021/summer_work/Graphs/fooddata", sep=';')
-print(incorrectData)
+#print(incorrectData)
 number_of_lines <- nrow(read.csv("/Users/larakallem/PycharmProjects/Summer2021/summer_work/Graphs/fooddata", sep=';'))
 
 errorWords <-incorrectData %>%
   filter(str_detect(WhereErrorOccurred, "Beginning|Middle|End"))
 
-  s <- paste("^", sound, sep="")
-  Words <- errorWords %>%
-    filter(str_detect(Word, s))
-  ggplot(Words, aes(x=WhereErrorOccurred, fill=Word)) +
+  ggplot(errorWords, aes(x=WhereErrorOccurred, fill=Word)) +
     geom_bar(stat="count") +
     scale_y_continuous(breaks=seq(0,number_of_lines,1)) +
     ggtitle(label="Session Feedback") +
@@ -27,4 +23,6 @@ errorWords <-incorrectData %>%
 
 }
 
-plotBME("c")
+plotBME()
+
+
