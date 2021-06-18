6d724ed88bdc4268a94252ead79270327347f78b
GDNW <sam.mccoy@my.simpson.edu>
2021-06-18 10:39:01 -0500

    GUI v2 fix

--- summer_work/LettersToCharactersTest.py
+++ summer_work/LettersToCharactersTest.py
@@ -1,6 +1,5 @@
 letters = ["ch", "sh", "th", "wh", "ng", "nk", "wr", "str", "spr"]
 
-input1 = input("Type: ")
 
 def lettersToCharacters(word):
     letters_dict = {"ch": "!", "sh": "@", "th": "#", "wh": "$", "ng": "%", "nk": "^", "wr": ")", "str": "*", "spr": "("}
@@ -10,8 +9,7 @@ def lettersToCharacters(word):
             word = word.replace(word[var: var+len(item)], letters_dict[item])
     return word
 
-out = lettersToCharacters(input1)
-print(out)
+
 
 def charactersToLetters(word):
     character_dict = {"!": "ch", "@": "sh", "#": "th", "$": "wh", "%": "ng", "^": "nk", ")": "wr", "*": "str", "(": "spr"}
@@ -21,5 +19,3 @@ def charactersToLetters(word):
             var = word.index(item)
             word = word.replace(word[var], character_dict[item])
     return word
-
-print(charactersToLetters(out))
