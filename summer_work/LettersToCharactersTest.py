7aa4a1f673866fd740efc1c859c028efd745a58a
zachg <zach.geery@my.simpson.edu>
2021-06-23 12:52:40 -0500

    updated blends

--- summer_work/LettersToCharactersTest.py
+++ summer_work/LettersToCharactersTest.py
@@ -1,21 +1,29 @@
-letters = ["ch", "sh", "th", "wh", "ng", "nk", "wr", "str", "spr"]
-
+letters = ["ch", "sh", "th", "wh", "ng", "nk", "wr", "str", "spr", "bl", "cl", "fl", "gl", "pl", "br", "cr", "dr", "fr",
+           "gr", "pr", "tr", "sk", "sl", "sp", "st", "sw"]
+digraph_dict = {"ch": "!", "sh": "@", "th": "#", "wh": "$", "ng": "%", "nk": "^", "wr": ")"}
+blend_dict = {"str": "*", "spr": "(", "bl":"[", "cl":"]", "fl":"|", "gl":":", "pl":"<", "br":">", "cr":"?", "dr":"~",
+                  "fr":"`", "gr":"\u00d8", "pr":"\u00d9", "tr":"\u00da", "sk":"\u00db", "sl":"\u00dd", "sp":"\u00de",
+                  "st":"\u00df", "sw":"\u00e0"}
+digraph_dict.update(blend_dict)
+reverse_dict = {value:key for(key, value) in digraph_dict.items()}
 
 def lettersToCharacters(word):
-    letters_dict = {"ch": "!", "sh": "@", "th": "#", "wh": "$", "ng": "%", "nk": "^", "wr": ")", "str": "*", "spr": "("}
+
     for item in letters:
         if item in word:
             var = word.index(item)
-            word = word.replace(word[var: var+len(item)], letters_dict[item])
+            word = word.replace(word[var: var+len(item)], digraph_dict[item])
     return word
-
 
 
 def charactersToLetters(word):
-    character_dict = {"!": "ch", "@": "sh", "#": "th", "$": "wh", "%": "ng", "^": "nk", ")": "wr", "*": "str", "(": "spr"}
-
-    for item in character_dict.keys():
+    for item in reverse_dict.keys():
         if item in word:
             var = word.index(item)
-            word = word.replace(word[var], character_dict[item])
+            word = word.replace(word[var], reverse_dict[item])
     return word
+
+out = input("Type here: ")
+print(lettersToCharacters(out))
+out1 = lettersToCharacters(out)
+print(charactersToLetters(out1))
