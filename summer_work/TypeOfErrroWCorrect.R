12d5055cf4f27196b158152a7d99f4ce79236dfc
larakallem <lara.kallem@my.simpson.edu>
2021-07-01 13:31:07 -0500

    adding function

---
+++ summer_work/TypeOfErrroWCorrect.R
@@ -0,0 +1,35 @@
+
+plotBar <- function(file) {
+   library(dplyr)
+   library(ggplot2)
+   library(stringr)
+   library(ggeasy)
+
+   fileName <- file
+   fileName2 <- paste("./summer_work/", fileName, sep ="")
+   fileNamecsv <- paste(fileName2, ".csv", sep ="")
+   pngName <- paste("TypeOfErrorWCorrect", fileName, sep = "")
+   addpng <- paste(pngName, ".png", sep ="")
+   png(addpng)
+
+number_of_lines <- nrow(read.csv(fileNamecsv, sep=';'))
+words <- read.csv(fileNamecsv, sep = ';')
+# errorWords <-incorrectData %>%
+#   filter(str_detect(WhereErrorOccurred))
+
+  Bar <- ggplot(words, aes(x=TypeOfError, fill=Word)) +
+    geom_bar(stat="count") +
+    scale_y_continuous(breaks=seq(0,number_of_lines,1)) +
+    ggtitle(label="Session Feedback") +
+    xlab(label="Type of Error") +
+    ylab(label="# of Words") +
+    ggeasy:: easy_center_title() +
+    labs(fill="Expected Word")
+
+print(Bar)
+dev.off()
+
+ggsave(addpng, path = "summer_work/Graphs", scale = 1)
+}
+
+plotBar("ErrorFile3")
